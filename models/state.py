0db77beef977ca99cad29b02ba4c2fdd011714bc
mdallen5393 <allen_matthew@outlook.com>
2022-10-25 19:44:37 +0000

    Minor changes; made do_update work for both file storage and db storage

--- models/state.py
+++ models/state.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from models.engine.db_storage import DBStorage
+# from models.__init__ import storage
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 import os
