7d59b192cf12d9fe4ac948a4c3684679c23ebf54
mdallen5393 <allen_matthew@outlook.com>
2022-10-25 16:56:42 +0000

    Task 6. Update City

--- models/city.py
+++ models/city.py
@@ -11,16 +11,19 @@ from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
+import os
 
 
 class City(BaseModel, Base):  # +T6: Base
     """ The city class, contains state ID and name """
-    __tablename__ = 'cities'
-    # state_id = "" -T6
-    state_id = Column(String(60), ForeignKey('states.id'),
-                      nullable=False)  # +T6
-    # name = ""  # -T6
-    name = Column(String(128), nullable=False)  # +T6
+    if os.getenv('HBNB_TYPE_STORAGE' == 'db'):
+        __tablename__ = 'cities'
+        state_id = Column(String(60), ForeignKey('states.id'),
+                          nullable=False)  # +T6
+        name = Column(String(128), nullable=False)  # +T6
+    else:
+        state_id = ""  # -T6
+        name = ""  # -T6
 
     places = relationship("Place", backref="cities",
                           cascade="all delete-orphan")
