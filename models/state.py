f04f1369eb5c814d2509013e5fa6c582217ab00f
mdallen5393 <allen_matthew@outlook.com>
2022-10-25 15:29:14 +0000

    Task 6. Update State

--- models/state.py
+++ models/state.py
@@ -1,8 +1,24 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+import os
 
 
-class State(BaseModel):
+class State(BaseModel, Base): # +T6: Base
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    # name = "" # -T6
+    name = Column(String(128), nullable=False) # +T6 vvv
+    storage = os.getenv('HBNB_TYPE_STORAGE')
+    if storage == 'db':
+        cities = relationship("")
+    if storage == 'file':
+        cities = getCities(self.id)
+
+    def getCities(self, id): # +T6
+        """Returns a list of City instances with state_id = id"""
+        cities = {}
+        for key, val in FileStorage.all('City').items():
+            if val.state_id == id:
+                cities[key] = val
+        return cities
