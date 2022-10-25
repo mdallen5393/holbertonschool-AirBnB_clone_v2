160ed4c8b9d8df3f8fd940c9caae66ac539f8405
mdallen5393 <allen_matthew@outlook.com>
2022-10-25 16:54:41 +0000

    Task 6. Update city

--- models/city.py
+++ models/city.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, DateTime, ForeignKey, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import BaseModel
 
 
-class City(BaseModel, Base): # +T6: Base
+class City(BaseModel):
     """ The city class, contains state ID and name """
-    __tablename__ = 'cities'
-    # state_id = "" -T6
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False) # +T6
-    # name = "" # -T6
-    name = Column(String(128), nullable=False) # +T6
+    state_id = ""
+    name = ""
