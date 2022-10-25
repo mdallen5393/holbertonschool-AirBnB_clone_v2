d95e113853ca8a3f4b7eedafc67802b00b3e86af
mdallen5393 <allen_matthew@outlook.com>
2022-10-25 17:01:17 +0000

    Task 6 Update State

--- models/state.py
+++ models/state.py
@@ -2,21 +2,22 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
 from models import storage
-from sqlalchemy import Column, DateTime, ForeignKey, Integer, String
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 import os
 
 
 class State(BaseModel, Base): # +T6: Base
     """ State class """
-    __tablename__ = 'states'
-    # name = "" # -T6
-    name = Column(String(128), nullable=False) # +T6 vvv
     storage = os.getenv('HBNB_TYPE_STORAGE')
     if storage == 'db':
-        cities = relationship("")
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False) # +T6 vvv
+        cities = relationship("City", backref="state",
+                               cascade="all delete-orphan")
     if storage == 'file':
         cities = cities()
+        name = "" # -T6
 
     @property
     def cities(self): # +T6
