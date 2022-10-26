9c39b3ea92a965fa06315ea60b1a0814e587b176
root <ben.sbanotto@gmail.com>
2022-10-26 15:02:36 -0700

    Reverted back previous changes

--- models/amenity.py
+++ models/amenity.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
+from models.base_model import BaseModel
 from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
 import os
 
 
@@ -10,7 +9,3 @@ class Amenity(BaseModel):
     """Amenity class, contains amenity name"""
     __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
-
-    # if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-    #     place_amenities = relationship('Place', secondary='place_amenity',
-    #                                    back_populates='amenities')
--- models/place.py
+++ models/place.py
@@ -7,17 +7,17 @@ from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
 from sqlalchemy.orm import relationship
 import os
 
-place_amenity = Table('place_amenity', Base.metadata,
-                      Column('place_id',
-                             String(60),
-                             ForeignKey('places.id'),
-                             primary_key=True,
-                             nullable=False),
-                      Column('amenity_id',
-                             String(60),
-                             ForeignKey('amenities_id'),
-                             primary_key=True,
-                             nullable=False))
+# place_amenity = Table('place_amenity', Base.metadata,
+#                       Column('place_id',
+#                              String(60),
+#                              ForeignKey('places.id'),
+#                              primary_key=True,
+#                              nullable=False),
+#                       Column('amenity_id',
+#                              String(60),
+#                              ForeignKey('amenities_id'),
+#                              primary_key=True,
+#                              nullable=False))
 
 
 class Place(BaseModel, Base):
@@ -36,13 +36,13 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, default=0, nullable=False)
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
-    amenity_ids = []
+    # amenity_ids = []
 
     if os.getenv('HBTN_TYPE_STORAGE') == 'db':
         reviews = relationship('Review', backref='place',
                                cascade='all, delete, delete-orphan')
-        amenities = relationship('Amenity', secondary='place_amenity',
-                                 backref='place_amenities', viewonly=False)
+        # amenities = relationship('Amenity', secondary='place_amenity',
+        #                          backref='place_amenities', viewonly=False)
 
     else:
         @property
@@ -54,22 +54,22 @@ class Place(BaseModel, Base):
                     reviews_list.append(review)
             return reviews_list
 
-        @property
-        def amenities(self):
-            """
-            Returns list of Amenity instances based on amenity_ids
-            that contains all Amenity.d linked to the Place
-            """
-            from models.amenity import Amenity
-            all_amenity = models.storage.all(Amenity)
-            amenities = [obj for obj in all_amenity.values()
-                         if obj.id == self.amenity_ids]
+        # @property
+        # def amenities(self):
+        #     """
+        #     Returns list of Amenity instances based on amenity_ids
+        #     that contains all Amenity.d linked to the Place
+        #     """
+        #     from models.amenity import Amenity
+        #     all_amenity = models.storage.all(Amenity)
+        #     amenities = [obj for obj in all_amenity.values()
+        #                  if obj.id == self.amenity_ids]
 
-        @amenities.setter
-        def amenities(self, obj):
-            """
-            Setter for amenities
-            """
-            from models.amenity import Amenity
-            if isinstance(obj, Amenity):
-                self.append(obj)
+        # @amenities.setter
+        # def amenities(self, obj):
+        #     """
+        #     Setter for amenities
+        #     """
+        #     from models.amenity import Amenity
+        #     if isinstance(obj, Amenity):
+        #         self.append(obj)
