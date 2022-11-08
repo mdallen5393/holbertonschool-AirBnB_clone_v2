b133d56005d61307d00ac88a64c17b299f50bd88
root <ben.sbanotto@gmail.com>
2022-11-08 13:03:19 -0800

    Updates to place.py to properly add to place_amenity table

--- models/place.py
+++ models/place.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-"""Place Module for HBNB project"""
+""" Place Module for HBNB project """
 from models.base_model import BaseModel, Base
 from models.review import Review
-import models
-from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
+from sqlalchemy import Column, Integer, String, Float, ForeignKey, Table
 from sqlalchemy.orm import relationship
-import os
+from os import getenv
 
 
 place_amenity = Table('place_amenity', Base.metadata,
@@ -20,10 +19,8 @@ place_amenity = Table('place_amenity', Base.metadata,
 
 
 class Place(BaseModel, Base):
-    """
-    Database vs File Storage Method for Place
-    """
-    __tablename__ = 'places'
+    """ A place to stay """
+    __tablename__ = "places"
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     name = Column(String(128), nullable=False)
@@ -36,27 +33,32 @@ class Place(BaseModel, Base):
     longitude = Column(Float, nullable=True)
     amenity_ids = []
 
-    if os.getenv('HBTN_TYPE_STORAGE') == 'db':
-        reviews = relationship('Review', backref='place',
-                               cascade='all, delete, delete-orphan')
-        amenities = relationship('Amenity',
+    storageType = getenv("HBNB_TYPE_STORAGE")
+    if storageType == "db":
+        reviews = relationship('Review',
+                               cascade="all, delete, delete-orphan",
+                               backref="place")
+        amenities = relationship("Amenity",
                                  secondary=place_amenity,
                                  back_populates='place_amenities',
                                  viewonly=False)
-
     else:
         @property
         def reviews(self):
-            """Send reviews to file storage"""
-            reviews_list = []
-            for review in models.storage.all(Review).values():
-                if review.place_id == self.id:
-                    reviews_list.append(review)
-            return reviews_list
+            """ getter for reviews in filestorage use"""
+            from models import storage
+            reviewList = []
+
+            for val in storage.all(Review).values():
+                if val.place_id == self.id:
+                    reviewList.append(val)
+            return reviewList
 
         @property
         def amenities(self):
-            """ getter for amenity table
+            """ getter for amenity table.
+                returns the list of Amenity instances where Amenity.id
+                is linked to Place
             """
             from models import storage
             from models.amenity import Amenity
@@ -64,7 +66,7 @@ class Place(BaseModel, Base):
             amenity_dict = storage.all(Amenity)
 
             for amenity_inst in amenity_dict.values():
-                if amenity_inst.id == self.amenity_ids:
+                if amenity_inst.id == self.amenity_id:
                     amenity_list.append(amenity_inst)
             return amenity_list
 
