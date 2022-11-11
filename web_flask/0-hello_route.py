db481f54a9576359de0aa4397356bd4d91adf6cc
mdallen5393 <allen_matthew@outlook.com>
2022-11-11 11:47:49 -0800

    Added `if __name__ == "__main__":`

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-# Script that starts a Flask web application
+"""
+Script that starts a Flask web application.
+"""
 from flask import Flask
 
 app = Flask(__name__)
@@ -11,3 +13,6 @@ def hello_HBNB():
     Prints `Hello HBNB!`.
     """
     return "Hello HBNB!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
