0e99bfb497bcfcee2a8a3e2b9ebf30899e886f8f
mdallen5393 <allen_matthew@outlook.com>
2022-11-11 11:29:51 -0800

    Added flask app

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 # Script that starts a Flask web application
-from flask import Flask, request
-from markupsafe import escape
+from flask import Flask
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 @app.route('/')
-def hello():
-    name = request.args.get("name", "World")
-    return f'Hello, {escape(name)}!'
+def hello_HBNB():
+    """
+    Prints `Hello HBNB!`.
+    """
+    return "Hello HBNB!"
