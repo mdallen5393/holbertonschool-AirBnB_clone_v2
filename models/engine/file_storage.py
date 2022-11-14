6cf3796ded62454c640be11d031dc03c0e205237
mdallen5393 <allen_matthew@outlook.com>
2022-11-14 06:59:07 -0800

    Task 5 & 6 redo using Jinja templates

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -69,3 +69,10 @@ class FileStorage:
         for key, value in dict(FileStorage.__objects).items():
             if value == obj:
                 del FileStorage.__objects[key]
+
+    def close(self): # p1170t7
+        """
+        Calls the reload() method for de-
+        serializing the JSON file to objects.
+        """
+        self.reload()
