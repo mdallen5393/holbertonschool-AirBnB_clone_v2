2781d3ad3d2379b2b67bf57c8f0300151756a8b0
mdallen5393 <allen_matthew@outlook.com>
2022-11-15 09:06:24 -0800

    Added files for task 11. HBNB filters

---
+++ web_flask/10-hbnb_filters.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Script that starts a Flask web application.
+"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/hbnb_filters")
+def show_state(id=None):
+    """
+    Displays an HTML page listing all `city`s inside
+    of `state`, sorted by `name`.
+    """
+    return render_template('6-index.html', states=storage.all(State),
+                           id=id)
+
+
+@app.teardown_appcontext
+def teardown(content):
+    """
+    Removes current SQLAlchemy Session.
+    """
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
