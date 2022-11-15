ddbeae46b2dc5f9fe82ae9047d7dacb52edac04f
mdallen5393 <allen_matthew@outlook.com>
2022-11-15 11:34:15 -0800

    Task 11. HBNB filters

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -5,19 +5,20 @@ Script that starts a Flask web application.
 from flask import Flask, render_template
 from models import storage
 from models.state import State
+from models.amenity import Amenity
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
 @app.route("/hbnb_filters")
-def show_state(id=None):
+def show_state():
     """
     Displays an HTML page listing all `city`s inside
     of `state`, sorted by `name`.
     """
-    return render_template('6-index.html', states=storage.all(State),
-                           id=id)
+    return render_template('10-hbnb_filters.html', states=storage.all(State),
+                           amenities=storage.all(Amenity))
 
 
 @app.teardown_appcontext
